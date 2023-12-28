6b900328cd6e002313cf58ba722da6e5caaa8daa
Jean Paul G <jpgiraldo7@gmail.com>
2023-12-28 11:20:58 -0500

    Completed frontend part of the app

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,22 +1,26 @@
 // src/App.tsx
 import { User } from './types';
+import UserDisplay from './components/UserDisplay';
 
 function App() {
   const users: User[] = [
     {
       name: 'Awesome User',
-      messages: [{ body: 'Here to break things' }, { body: 'Smash smash' }],
+      messages: [
+        { body: 'Here to break things' },
+        { body: 'Smash smash' },
+        { body: 'Third message' },
+      ],
     },
   ];
 
   return (
     // src/App.tsx
-
-    // ...
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
-      <h2 className="text-4xl text-yellow-500">Hello World!</h2>
+      {users.map((user, i) => (
+        <UserDisplay user={user} key={i} />
+      ))}
     </div>
-    // ...
   );
 }
 
--- react-client/src/components/UserDisplay.tsx
+++ react-client/src/components/UserDisplay.tsx
@@ -1,5 +1,23 @@
-function UserDisplay() {
-  return <p>User Component</p>;
+import MessageDisplay from './MessageDisplay';
+import type { User } from '../types';
+
+type Props = {
+  user: User;
+};
+
+function UserDisplay({ user }: Props) {
+  return (
+    <div className="flex gap-x-24 justify-center">
+      <div className="rounded-md flex justify-center items-center drop-shadow-md bg-neutral-700 w-48 h-20">
+        <p className="text-xl text-gray-200 font-bold">{user.name}</p>
+      </div>
+      <div>
+        {user.messages.map((message, i) => (
+          <MessageDisplay key={i} index={i} message={message} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default UserDisplay;
