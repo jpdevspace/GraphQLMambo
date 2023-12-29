f349a57ba73216afe7972d5d422abe8d528c7f7f
Jean Paul G <jpgiraldo7@gmail.com>
2023-12-28 23:38:26 -0500

    Creating backend, Prisma setup

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -15,7 +15,6 @@ function App() {
   ];
 
   return (
-    // src/App.tsx
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
       {users.map((user, i) => (
         <UserDisplay user={user} key={i} />
