d4e4bfb597616a8ef1905f9edce150e8cee3525b
Jean Paul G <jpgiraldo7@gmail.com>
2023-12-29 00:26:24 -0500

    added graphql-yoga

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hey there! 👋');
+import { createServer } from 'node:http';
+import { createSchema, createYoga } from 'graphql-yoga';
+import { create } from 'node:domain';
+
+const port = Number(process.env.API_PORT) || 4000;
+
+const schema = createSchema({
+  typeDefs: `
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => 'world'
+    }
+  }
+});
+
+const yoga = createYoga({ schema });
+const server = createServer(yoga);
+
+server.listen(port, () => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});
