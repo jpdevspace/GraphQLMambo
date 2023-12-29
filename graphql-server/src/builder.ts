1c71f15d7e86713a8cf05c7143e8615cbcea2d4e
Jean Paul G <jpgiraldo7@gmail.com>
2023-12-29 16:49:17 -0500

    Added pothos for type definition with Prisma

---
+++ graphql-server/src/builder.ts
@@ -0,0 +1,26 @@
+import SchemaBuilder from '@pothos/core';
+import { DateResolver } from 'graphql-scalars';
+import PrismaPlugin from '@pothos/plugin-prisma';
+import type PrismaTypes from '@pothos/plugin-prisma/generated';
+import { prisma } from './db';
+
+type ScalarDate = {
+  Scalars: {
+    Date: { 
+      Input: Date; 
+      Output: Date
+    }
+  };
+
+  PrismaTypes: PrismaTypes;
+}
+
+const config = { 
+  plugins: [PrismaPlugin],
+  prisma: { client: prisma }
+
+}
+
+export const builder = new SchemaBuilder<ScalarDate>(config);
+
+builder.addScalarType('Date', DateResolver);
